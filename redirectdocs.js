4549bdff62add1a1d1fdffb82072562b41a251f3
mchristofides <michael@Michaels-Air.lan>
2020-04-24 18:04:03 +0100

    Avoid infinite redirect on root docs page

--- redirectdocs.js
+++ redirectdocs.js
@@ -8,6 +8,10 @@ function redirectdocs(details) {
         console.log(`pg_docs_bot: directing to ${pgdocsversion}`);
         return {cancel: false};
     } 
+    else if (details.url === 'https://www.postgresql.org/docs/') {
+        //Avoid infinite redirect on root docs page
+        return {cancel: false};
+    } 
     else {
         console.log('pg_docs_bot: checking link');
         details.url = details.url.replace(/current|7\.2|7\.3|7\.4|8\.0|8\.1|8\.2|8\.3|8\.4|9\.0|9\.1|9\.2|9\.3|9\.4|9\.5|9\.6|10|11|12|13/, pgdocsversion);
