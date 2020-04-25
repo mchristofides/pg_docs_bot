59fa0d7b6f275a5457bfddf04b08352782be2609
mchristofides <michael@Michaels-Air.lan>
2020-04-25 14:08:44 +0100

    Reduce width of notice a little

--- pgdocsbotnotice.js
+++ pgdocsbotnotice.js
@@ -1,3 +1,3 @@
 var hover = document.createElement('Hover');
-hover.innerHTML = '<div style="font-size: 14px;color: #0D0A0B;position: fixed;bottom: 30px;right: 30px;padding: 20px 20px 20px 20px;max-width: 350px;background: #BBCED9;border-radius: 5px;z-index: 100;box-shadow: 5px 5px 5px rgba(0,0,0,0.2)">You have <a href="https://github.com/mchristofides/pg_docs_bot/" target="_blank" style="color: #336791;"><u>pg_docs_bot</u></a> installed, so may have been redirected to this page.</div>';
+hover.innerHTML = '<div style="font-size: 14px;color: #0D0A0B;position: fixed;bottom: 30px;right: 30px;padding: 20px 20px 20px 20px;max-width: 300px;background: #BBCED9;border-radius: 5px;z-index: 100;box-shadow: 5px 5px 5px rgba(0,0,0,0.2)">You have <a href="https://github.com/mchristofides/pg_docs_bot/" target="_blank" style="color: #336791;"><u>pg_docs_bot</u></a> installed, so may have been redirected to this page.</div>';
 document.body.appendChild(hover);
