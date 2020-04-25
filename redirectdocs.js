16fa560aa777e86628ea4d8cac0ceb0150cd7c16
mchristofides <michael@Michaels-Air.lan>
2020-04-25 16:48:23 +0100

    Drop support for 7.x and 8.x versions
    Several deprecated features in these versions, and they're hardly ever indexed so didn't seem worth working around them.

--- redirectdocs.js
+++ redirectdocs.js
@@ -34,7 +34,7 @@ function redirectdocs(details) {
     } 
     else {
         console.log('pg_docs_bot: checking link');
-        details.url = details.url.replace(/current|7\.2|7\.3|7\.4|8\.0|8\.1|8\.2|8\.3|8\.4|9\.0|9\.1|9\.2|9\.3|9\.4|9\.5|9\.6|10|11|12|13/, pgdocsversion);
+        details.url = details.url.replace(/current|9\.0|9\.1|9\.2|9\.3|9\.4|9\.5|9\.6|10|11|12|13/, pgdocsversion);
         return {redirectUrl: details.url};
     }
   };
